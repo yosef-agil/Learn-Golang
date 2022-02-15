066ad4e77185cb3efe0a4ad43d5fc0b95bfff27e
yosef-agil <yosefagil8@gmail.com>
2022-02-15 17:34:03 +0700

    New line codes

--- BookingApp/main.go
+++ BookingApp/main.go
@@ -8,9 +8,8 @@ func main(){
 	// Setelah mendeklarasi kan variable, var tersebut harus digunakan
 	conferenceName := "Go Conference"
 	const conferenceTickets int = 50
-	var remainingTickets int =50
+	var remainingTickets uint =50
 
-	remainingTickets = -1
 	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 
 	// %v untuk memberitahu go untuk mengganti dengan variable
@@ -18,11 +17,23 @@ func main(){
 	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var userName string
-	var userTickets int
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
 	// ask user for their name
+	// user inputs
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+	fmt.Println("Enter your email: ")
+	fmt.Scan(&email)
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
 
-	userName = "Tom"
-	userTickets = 2
-	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+	remainingTickets = remainingTickets - userTickets
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 }
